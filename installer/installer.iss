9bf25cbcf0a4d8e5495882f31f00a33818bcdbed
Alex Sim <ale.simula97@gmail.com>
2022-03-27 19:25:23 +0200

    Update version

--- installer/installer.iss
+++ installer/installer.iss
@@ -22,7 +22,7 @@
 #define path_assoc_default ".DEFAULT\"+path_assoc_user 
 
 [Setup]
-AppVersion=1.3.3
+AppVersion=1.3.4
 PrivilegesRequired=admin
 AppName=WSA PacMan
 AppPublisher=alesimula
--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ import 'dart:async';
 import 'theme.dart';
 
 const String appTitle = 'WSA Package Manager';
-const String appVersion = '1.3.3';
+const String appVersion = '1.3.4';
 
 late bool darkMode;
 
